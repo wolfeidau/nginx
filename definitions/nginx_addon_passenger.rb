8c33a0f23c3ae5fe7df48a9d56a4af296afcea68
Gudleik Rasch <gudleik@gmail.com>
2012-06-26 11:05:01 +0200

    absolute path to passenger-config

--- definitions/nginx_addon_passenger.rb
+++ definitions/nginx_addon_passenger.rb
@@ -1,7 +1,7 @@
 define :nginx_addon_passenger do
   gem_package "passenger"
 
-  node.set[:nginx][:passenger_root] = `passenger-config --root`.chomp
+  node.set[:nginx][:passenger_root] = `/usr/bin/passenger-config --root`.chomp
   node.set[:nginx][:passenger_ruby] = "/usr/bin/ruby"
 
   passenger_path = node[:nginx][:passenger_root] + "/ext/nginx"
